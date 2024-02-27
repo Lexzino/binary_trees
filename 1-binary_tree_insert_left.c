78ce40e5db4fd959b87d1e6c4c8c2926aa2bfbe0
Lexzino <alexudeze001@gmal.com>
2024-02-27 06:47:48 -0800

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -24,7 +24,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new->parent = parent;
 	new->right = NULL;
 	new->left = parent->left;
-	parent->left = new_node;
+	parent->left = new;
 	if (new->left)
 		new->left->parent = new;
 
